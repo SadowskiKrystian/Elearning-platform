03bdcba988476e1237b6a5e0d90a02444afeb410
AleksanderBondar <darkduble12@gmail.com>
2023-05-04 14:41:48 +0200

    :rocket: Decorations part 2.

--- src/main/resources/static/js/fetchNews.js
+++ src/main/resources/static/js/fetchNews.js
@@ -21,14 +21,21 @@ function findResults(newses){
         fillRow(news);
     });
 }
-function fillRow(news){
+function fillRow(news) {
+    let date = new Intl.DateTimeFormat('en-US', { weekday: 'short', month: 'long', year: 'numeric' }).format(new Date(news.createDate));
     $('#informations_wrapper').append(
-        '<div class="information">' +
-            '<div class="information_title">' + news.title +
-            '</div>' +
-            '<div class="information_content">' + news.content +
-            '</div>' +
-            '<div class="information_date">' + news.createDate +
+        '<div class="information_basis">' +
+            '<div class="information">'+
+                '<div class="information_title">' +
+                '<p>Created: </p>'+
+                `<h3>${news.title}</h3>` +
+                '</div>' +
+                '<div class="information_date">' + 
+                `<span>${date}</span>` +
+                '</div>' +
+                '<div class="information_content">' + 
+                news.content +
+                '</div>' +
             '</div>' +
         '</div>'
     );
